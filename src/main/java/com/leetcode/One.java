69fc246ea8d4417d26391d5ff58c17c97cfdb823
Taowd <Taowd@Taowd-PC>
2019-01-14 22:33:31 +0800

    新增解决方案

--- src/main/java/com/leetcode/One.java
+++ src/main/java/com/leetcode/One.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.alibaba.fastjson.JSON;
 
 /**
@@ -20,8 +23,16 @@ public class One {
 
 		System.out.println(JSON.toJSONString(result));
 
+		System.out.println(JSON.toJSONString(new One().twoSumNew(nums, target)));
+
 	}
 
+	/**
+	 * 暴力法很简单。遍历每个元素 xxx，并查找是否存在一个值与 target−xtarget - xtarget−x 相等的目标元素。
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
 	public int[] twoSum(int[] nums, int target) {
 		int[] result = new int[2];
 		for (int i = 0; i < nums.length; i++) {
@@ -33,5 +44,28 @@ public class One {
 			}
 		}
 		return result;
+	}
+
+	/**
+	 * 一个简单的实现使用了两次迭代。在第一次迭代中，我们将每个元素的值和它的索引添加到表中。然后，在第二次迭代中，我们将检查每个元素所对应的目标元素（target−nums[i]target
+	 * - nums[i]target−nums[i]）是否存在于表中。注意，该目标元素不能是 nums[i]nums[i]nums[i] 本身！<br/>
+	 * 空间换时间 效率更高.
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSumNew(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i);
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement) && map.get(complement) != i) {
+				return new int[] { i, map.get(complement) };
+			}
+		}
+		throw new IllegalArgumentException("No tow sum solution");
 	}
 }
