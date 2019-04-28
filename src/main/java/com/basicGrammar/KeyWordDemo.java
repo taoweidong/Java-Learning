5379e301cb79cdad3b7bbfbc6475d0209a0cbe45
Taowd <taowd@outlook.com>
2019-04-28 22:18:19 +0800

    基础学习

--- src/main/java/com/basicGrammar/KeyWordDemo.java
+++ src/main/java/com/basicGrammar/KeyWordDemo.java
@@ -1,16 +1,20 @@
 package com.basicGrammar;
 
 /*
- * 关键字：被java语言赋予特定含义的单词。
- * 特点：组成关键字单词的字母全部小写。
- * 注意：
- * A:goto和const是保留字
- * B:类似于Notepad++这样的高级记事本，针对关键字都有特殊的颜色标记。
+ * 关键字：被java语言赋予特定含义的单词。<br>
+ * 特点：组成关键字单词的字母全部小写。<br>
+ * 注意：<br>
+ * A:goto和const是保留字<br>
+ * B:类似于Notepad++这样的高级记事本，针对关键字都有特殊的颜色标记。<br>
  */
 class KeyWordDemo {
 
 	public static void main(String[] args) {
 
 		System.out.println("HelloWorld");
+
+		// 先定义 后初始化 再使用
+		// int sum;
+		// int suu = sum + 10; 错误用法
 	}
 }
