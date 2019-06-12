fd84a04b54676a59240b04143d0493b2c7539caa
Taowd <taowd@outlook.com>
2019-06-12 22:53:33 +0800

    线程池中使用Callable

--- src/main/java/com/juc/TestThreadPool.java
+++ src/main/java/com/juc/TestThreadPool.java
@@ -1,7 +1,9 @@
 package com.juc;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * 线程池：提供了一个线程队列，队列中保存着所有等待状态的线程。避免了创建和销毁线程的开销
@@ -16,11 +18,44 @@ public class TestThreadPool {
 		// 1.创建线程池
 		ExecutorService pool = Executors.newFixedThreadPool(5);
 		// 2.为线程池中的线程分配任务
+		List<Future<Integer>> futureList = Lists.newArrayList();
 		for (int i = 0; i < 10; i++) {
-			pool.submit(threadPoolDemo);
+			Future<Integer> future = pool.submit(new Callable<Integer>() {
+
+				@Override
+				public Integer call() throws Exception {
+
+					int sum = 0;
+					for (int j = 0; j <= 100; j++) {
+						sum += j;
+					}
+					return sum;
+				}
+			});
+			futureList.add(future);
 		}
 		// 3.关闭线程池
 		pool.shutdown();
+
+		futureList.forEach(x -> {
+			try {
+				System.out.println(x.get());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+
+		});
+
+		// // 1.创建线程池
+		// ExecutorService pool = Executors.newFixedThreadPool(5);
+		// // 2.为线程池中的线程分配任务
+		// for (int i = 0; i < 10; i++) {
+		// pool.submit(threadPoolDemo);
+		// }
+		// // 3.关闭线程池
+		// pool.shutdown();
 
 	}
 }
