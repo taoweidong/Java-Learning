b1fe06d3b707bad37a7cdaf35b1bea30f6cf00b5
Taowd <taowd@outlook.com>
2019-07-17 22:20:19 +0800

    类初始化分析

--- src/main/java/com/singleton/Singleton3.java
+++ src/main/java/com/singleton/Singleton3.java
@@ -6,21 +6,32 @@ package com.singleton;
  * @Description 双重锁的形式:这个模式将同步内容下方到if内部，提高了执行的效率，不必每次获取对象时都进行同步，只有第一次才同步，创建了以后就没必要了。
  */
 public class Singleton3 {
-    private static volatile Singleton3 instance = null;
-    /**
-     * 私有化构造函数
-     */
-    private Singleton3() {
-    }
 
-    public static Singleton3 getInstance() {
-        if (instance == null) {
-            synchronized (Singleton3.class) {
-                if (instance == null) {
-                    instance = new Singleton3();
-                }
-            }
-        }
-        return instance;
-    }
+	/**
+	 * 设置为内存可见性
+	 */
+	private static volatile Singleton3 instance = null;
+
+	/**
+	 * 私有化构造函数
+	 */
+	private Singleton3() {
+
+	}
+
+	/**
+	 * 提供外部调用
+	 * @return 实例对象
+	 */
+	public static Singleton3 getInstance() {
+
+		if (instance == null) {
+			synchronized (Singleton3.class) {
+				if (instance == null) {
+					instance = new Singleton3();
+				}
+			}
+		}
+		return instance;
+	}
 }
