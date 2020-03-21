ea134bfa71ff563e80af7807e3a186123eb93c5f
Taoweidong <taowd@outlook.com>
2020-03-21 09:37:37 +0800

    新增快速排序算法实现

---
+++ src/main/java/com/data/structure/QuickSort.java
@@ -0,0 +1,81 @@
+package com.data.structure;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 快速排序实现
+ * @author Taoweidong
+ */
+public class QuickSort {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 2, 3, 56, 24, 12, 9, 85, 533, 62 };
+		System.out.println(JSON.toJSONString(arr));
+		quickSort(arr, 0, arr.length - 1);
+		System.out.println(JSON.toJSONString(arr));
+
+	}
+
+	/**
+	 * 快速排序算法
+	 * @param arr   待排序的数组
+	 * @param left  左边区域
+	 * @param right 右边区域
+	 */
+	public static void quickSort(int[] arr, int left, int right) {
+
+		if (left >= right) {
+			return;
+		}
+		//计算基准数据的位置
+		int partition = partition(arr, left, right);
+		//递归对左边进行快速排序
+		quickSort(arr, left, partition - 1);
+		//递归对右边进行排序
+		quickSort(arr, partition + 1, right);
+
+	}
+
+	/**
+	 * 核心逻辑：将给定数组的给定区间进行快速排序，并返回排序后基准值得为之，为后续的分区间迭代排序提供条件
+	 * @param arr   待排序数据
+	 * @param left  左边区间
+	 * @param right 右边区间
+	 * @return 基准值得位置
+	 */
+	private static int partition(int[] arr, int left, int right) {
+		//将第一个数作为基准值。挖坑 设置基准值：初始基准值为最左边的数据
+		int partition = arr[left];
+
+		//使用循环实现分区操作
+		while (left < right) {
+
+			//从右向左移动j，找到第一个小于基准值的值 arr[right]
+			while (left < right && arr[right] >= partition) {
+				--right;
+			}
+
+			if (left < right) {
+				arr[left] = arr[right];
+				++left;
+			}
+
+			//从左向右移动i，找到第一个大于等于基准值的值 arr[left]
+			while (left < right && arr[left] < partition) {
+				left++;
+			}
+
+			if (left < right) {
+				arr[right] = arr[left];
+				--right;
+			}
+		}
+
+		//使用基准值填坑，这就是基准值的最终位置
+		arr[left] = partition;
+		//返回基准值的位置索引
+		return left;
+	}
+
+}
--- src/main/java/com/nowcoder/ReverseList_Test.java
+++ src/main/java/com/nowcoder/ReverseList_Test.java
@@ -8,82 +8,90 @@ package com.nowcoder;
  * @Version V1.0
  */
 public class ReverseList_Test {
-    //定义链表结构
-    public static class ListNode {
-        /**
-         * 值
-         */
-        int val;
-        /**
-         * 后一个节点
-         */
-        ListNode next = null;
 
-        /**
-         * 构造函数
-         *
-         * @param val
-         */
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
+	//定义链表结构
+	public static class ListNode {
 
-    public static void main(String[] args) {
-        ListNode node = new ListNode(12);
-        ListNode node1 = new ListNode(34);
-        ListNode node2 = new ListNode(45);
-        ListNode node3 = new ListNode(55);
+		/**
+		 * 值
+		 */
+		int val;
 
-        node.next = node1;
-        node1.next = node2;
-        node2.next = node3;
+		/**
+		 * 后一个节点
+		 */
+		ListNode next = null;
 
-        //遍历链表数据
-        ListNode n = node;
-        while (n != null) {
-            if (n.next != null) {
-                System.out.print(n.val + "-->");
-            } else {
-                System.out.print(n.val);
-            }
-            n = n.next;
-        }
-        System.out.println();
+		/**
+		 * 构造函数
+		 * @param val
+		 */
+		ListNode(int val) {
 
+			this.val = val;
+		}
+	}
 
-        ListNode n2 = ReverseList(node);
-        while (n2 != null) {
-            if (n2.next != null) {
-                System.out.print(n2.val + "-->");
-            } else {
-                System.out.print(n2.val);
-            }
-            n2 = n2.next;
-        }
-        System.out.println();
+	public static void main(String[] args) {
 
-        System.out.println();
-    }
+		ListNode node = new ListNode(12);
+		ListNode node1 = new ListNode(34);
+		ListNode node2 = new ListNode(45);
+		ListNode node3 = new ListNode(55);
 
-    public static ListNode ReverseList(ListNode head) {
-        if (head == null) return null;
-        ListNode pre = null;//当前节点的前一个节点
-        ListNode next = null;//当前节点的后一个节点
+		node.next = node1;
+		node1.next = node2;
+		node2.next = node3;
 
-        while (head != null) {
-            //先用next存储head的下一个节点的信息，保证单链表不会因为失去head节点的原next节点而因此断裂
-            next = head.next;
-            //保存完next，就可以让head从指向next转向指向pre
-            head.next = pre;
-            //head指向pre后，就可以继续下一个节点
-            //让pre head next依次向后移动，每移动到一个节点时进行指针反转操作
-            pre = head;
-            head = next;
-        }
-        //如果head为null的时候，pre就为最后一个节点了，但是链表已经反转完毕，pre就是反转后链表的第一个节点
-        //直接输出pre就是我们想要得到的反转后的链表
-        return pre;
+		//遍历链表数据
+		ListNode n = node;
+		while (n != null) {
+			if (n.next != null) {
+				System.out.print(n.val + "-->");
+			} else {
+				System.out.print(n.val);
+			}
+			n = n.next;
+		}
+		System.out.println();
 
-    }
+		ListNode n2 = ReverseList(node);
+		while (n2 != null) {
+			if (n2.next != null) {
+				System.out.print(n2.val + "-->");
+			} else {
+				System.out.print(n2.val);
+			}
+			n2 = n2.next;
+		}
+		System.out.println();
+
+		System.out.println();
+	}
+
+	public static ListNode ReverseList(ListNode head) {
+
+		if (head == null) {
+			return null;
+		}
+		//当前节点的前一个节点
+		ListNode pre = null;
+		//当前节点的后一个节点
+		ListNode next = null;
+
+		while (head != null) {
+			//先用next存储head的下一个节点的信息，保证单链表不会因为失去head节点的原next节点而因此断裂
+			next = head.next;
+			//保存完next，就可以让head从指向next转向指向pre
+			head.next = pre;
+			//head指向pre后，就可以继续下一个节点
+			//让pre head next依次向后移动，每移动到一个节点时进行指针反转操作
+			pre = head;
+			head = next;
+		}
+		//如果head为null的时候，pre就为最后一个节点了，但是链表已经反转完毕，pre就是反转后链表的第一个节点
+		//直接输出pre就是我们想要得到的反转后的链表
+		return pre;
+
+	}
 }
