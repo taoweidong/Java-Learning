055ee5c984f4aeee8d99338d12a7d9c79d32f42e
Taoweidong <taowd@outlook.com>
2020-03-23 23:35:40 +0800

    完全背包问题

--- src/main/java/com/data/structure/KnapsackProblem.java
+++ src/main/java/com/data/structure/KnapsackProblem.java
@@ -3,7 +3,9 @@ package com.data.structure;
 import java.util.Scanner;
 
 /**
- * 动态规划-背包问题
+ * 动态规划-0-1背包问题<br/>
+ * 问题描述：有 N 件物品和一个容量是 V 的背包。每件物品只能使用一次。第 i 件物品的体积是 vi，价值是 wi。
+ * 求解将哪些物品装入背包，可使这些物品的总体积不超过背包容量，且总价值最大。输出最大价值。
  * @author Taoweidong
  */
 public class KnapsackProblem {
--- src/main/java/com/data/structure/KnapsackProblem2.java
+++ src/main/java/com/data/structure/KnapsackProblem2.java
@@ -1,82 +1,65 @@
 package com.data.structure;
 
+import java.util.Scanner;
+
 /**
- * 动态规划-背包问题2-优化版本，记录详细信息
+ * 动态规划-完全背包问题<br/>
+ * 问题描述：有 N 种物品和一个容量是 V 的背包，每种物品都有无限件可用。第 i 种物品的体积是 vi，价值是 wi 。
+ * 求解将哪些物品装入背包，可使这些物品的总体积不超过背包容量，且总价值最大。输出最大价值。
  * @author Taoweidong
  */
 public class KnapsackProblem2 {
 
 	public static void main(String[] args) {
-
-		//物品的重量
-		int[] w = { 1, 4, 3 };
-		//物品的价值
-		int[] value = { 1500, 3000, 2000 };
-		//背包的最大容量
-		int m = 4;
+		//1、从输入流中获取数据
+		Scanner sc = new Scanner(System.in);
 		//物品的个数
-		int n = value.length;
+		int N = sc.nextInt();
+		//背包的容积
+		int V = sc.nextInt();
+		//物品的体积:第i个元素表示第i个物品的体积
+		int[] v = new int[N + 1];
+		//物品的价值:第i个元素表示第i个物品的价值
+		int[] w = new int[N + 1];
 
-		//创建动态规划分布表
-		int[][] v = new int[n + 1][m + 1];
-		//初始化分布表
-		for (int i = 0; i < v.length; i++) {
-			v[i][0] = 0;
-		}
-		for (int j = 0; j < v[0].length; j++) {
-			v[0][j] = 0;
+		for (int i = 1; i <= N; i++) {
+			v[i] = sc.nextInt();
+			w[i] = sc.nextInt();
 		}
 
-		//创建动态规划分布表
-		int[][] path = new int[n + 1][m + 1];
+		//2、开始解析问题
+		//表示前i个物品最大容量为j时的最大价值
+		int[][] dp = new int[N + 1][V + 1];
+		//初始化基本数据
+		for (int i = 0; i < dp.length; i++) {
+			dp[i][0] = 0;
+		}
+		for (int i = 0; i < dp.length; i++) {
+			dp[0][1] = 0;
+		}
 
 		//开始进行动态规划
-		for (int i = 1; i < v.length; i++) {
-			for (int j = 1; j < v[i].length; j++) {
-				//如果新增的商品大于当前背包容量j的话，就无法放入背包，则当前的价值仍然为前i-1个商品的价值
-				if (w[i - 1] > j) {
-					v[i][j] = v[i - 1][j];
-				} else {
-					//否则，新增的商品容量小于当前背包容量j，则说明可以添加到背包中
-					if (v[i - 1][j] < value[i - 1] + v[i - 1][j - w[i - 1]]) {
-						v[i][j] = value[i - 1] + v[i - 1][j - w[i - 1]];
-						path[i][j] = 1;
-					} else {
-						v[i][j] = v[i - 1][j];
+		//max表示前i-1种物品中选取若干件物品放入剩余空间为j-k*v[i]的背包中所能得到的最大价值加上k件第i种物品；
+		int max = 0;
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[i].length; j++) {
+
+				//此处循环计算max的值，最大的值即为当前节点的最大价值
+				for (int k = 0; k * v[i] <= j; k++) {
+					//选择k件相同物品时重量最大为j的最大价值价值
+					int t = dp[i - 1][j - k * v[i]] + k * w[i];
+					if (max < t) {
+						max = t;
 					}
 				}
+				dp[i][j] = max;
+				//特别要注意max要重置为零,否则dp数组里面的值是错误的
+				max = 0;
 			}
-
 		}
 
-		int maxValue = 0;
-		for (int i = 0; i < v.length; i++) {
-			for (int j = 0; j < v[0].length; j++) {
-				maxValue = Math.max(maxValue, v[i][j]);
-				System.out.print(v[i][j] + "\t");
-			}
-			System.out.println();
-		}
-
-		System.out.println("结果：" + maxValue);
-
-		for (int i = 1; i < path.length; i++) {
-			for (int j = 1; j < path[0].length; j++) {
-				System.out.print(path[i][j] + "\t");
-			}
-			System.out.println();
-		}
-
-		int i = path.length - 1;
-		int j = path[0].length - 1;
-		//从记录的最后开始找
-		while (i > 0 && j > 0) {
-			if (path[i][j] == 1) {
-				System.out.printf("第%d个商品放入到背包\n", i);
-				j = j - w[i - 1];
-			}
-			i--;
-		}
+		//3、输出最后一个节点的值：也就是N个背包在容量为V的限制下的最大价值
+		System.out.println(dp[N][V]);
 
 	}
 
