7534d81d4e692060f315b23557029a14d85022db
Taoweidong <taowd@outlook.com>
2020-04-02 00:02:52 +0800

    变量替换

---
+++ src/main/java/com/data/structure/HuaWei_Test20200401_02.java
@@ -0,0 +1,66 @@
+package com.data.structure;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.*;
+
+/**
+ * 给出一个仅包含字母的字符串，不包含空格，统计字符串中各个字母出现的次数，并按照字母出现的次数从大到小
+ * 的顺序输出各个字母及其出现的次数信息，如果次数相同，按照自然顺序进行排序
+ * @author Taoweidong
+ */
+public class HuaWei_Test20200401_02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			//输入参数
+			String str = sc.nextLine();
+
+			//统计字符出现的个数
+			Data[] datas = new Data[28];
+			for (Character item : str.toCharArray()) {
+				Data data;
+				if (datas[item - 'a'] != null) {
+					data = datas[item - 'a'];
+					data.count++;
+				} else {
+					data = new Data();
+					data.ch = item.toString();
+					data.count = 1;
+
+				}
+				datas[item - 'a'] = data;
+
+			}
+
+			//排序
+			for (int i = 0; i < datas.length; i++) {
+				for (int j = 0; j < datas.length - i - 1; j++) {
+					if (datas[j].count < datas[j + 1].count) {
+						Data temp = datas[j];
+						datas[j] = datas[j + 1];
+						datas[j + 1] = temp;
+					}
+				}
+			}
+
+			System.out.println(JSON.toJSONString(datas));
+
+		}
+	}
+}
+
+class Data {
+
+	/**
+	 * 字符
+	 */
+	public String ch;
+
+	/**
+	 * 数量
+	 */
+	public Integer count;
+}
--- src/main/java/com/leetcode/leetcode/editor/cn/NumberOfIslands.java
+++ src/main/java/com/leetcode/leetcode/editor/cn/NumberOfIslands.java
@@ -28,6 +28,15 @@ package com.leetcode.leetcode.editor.cn;
 
 /**
  * 岛屿数量
+ * 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设
+ * 网格的四个边均被水包围。
+ * 示例 1:
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 输出: 1
  * @author Taoweidong
  */
 public class NumberOfIslands {
