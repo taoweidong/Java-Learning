a52a6096cc6af0a9bfd5e16c892378c189b7dcd6
Taoweidong <taowd@outlook.com>
2020-04-18 00:01:51 +0800

    电话号码的字母组合

--- src/main/java/com/leetcode/leetcode/editor/cn/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/com/leetcode/leetcode/editor/cn/LetterCombinationsOfAPhoneNumber.java
@@ -17,8 +17,7 @@
 
 package com.leetcode.leetcode.editor.cn;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 电话号码的字母组合
@@ -28,12 +27,60 @@ import java.util.List;
 public class LetterCombinationsOfAPhoneNumber {
     public static void main(String[] args) {
         Solution solution = new LetterCombinationsOfAPhoneNumber().new Solution();
+        System.out.println(solution.letterCombinations("25"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<String> letterCombinations(String digits) {
-            return Collections.EMPTY_LIST;
+            //结果
+            List<String> result = new ArrayList<>();
+
+            if (digits == null || digits.length() == 0) {
+                return Collections.EMPTY_LIST;
+            }
+
+            //字典
+            Map<String, String> map = new HashMap<>(8);
+            map.put("2", "abc");
+            map.put("3", "def");
+            map.put("4", "ghi");
+            map.put("5", "jkl");
+            map.put("6", "mno");
+            map.put("7", "pqrs");
+            map.put("8", "tuv");
+            map.put("9", "wxyz");
+
+            result.addAll(backtrack("", digits, map));
+
+
+            return result;
+        }
+
+        /**
+         * 递归回溯
+         *
+         * @param s
+         * @param digits
+         * @param map
+         * @return
+         */
+        private List<String> backtrack(String s, String digits, Map<String, String> map) {
+            List<String> result = new ArrayList<>();
+            //递归退出条件
+            if (digits == null || digits.length() == 0) {
+                result.add(s);
+                return result;
+            }
+            String letter = map.get(digits.substring(0, 1));
+
+            for (int i = 0; i < letter.length(); i++) {
+                String str = letter.substring(i, i + 1);
+                //回溯
+                backtrack(s + str, digits.substring(1), map);
+            }
+
+            return result;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
