bda9b35be501e49c7f5514b9e854c9b40c5b2419
Taoweidong <taowd@outlook.com>
2020-04-19 22:34:46 +0800

    跳跃比赛代码优化

--- src/main/java/com/huawei/HuaWei_Test20200330_01.java
+++ src/main/java/com/huawei/HuaWei_Test20200330_01.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
  * 跳跃比赛
  * 2 2 3 6 8 9
  * 2
+ * https://leetcode-cn.com/problems/jump-game-ii/solution/hui-su-dp-yi-ji-tan-xin-jie-fa-by-liuchuan1992/
+ * 贪心算法解决：贪心的思路在于每次确定一个层级或者上下界。然后根据上下界确定下一个循环的范围，这里要注意的是可能会存在跳不过去的场景，虽然题目没有说，
+ * 我们假设如果最终不能跳到最后一个位置，则返回-1 (1,0,0,0,4) -> -1
  *
  * @author Taoweidong
  */
