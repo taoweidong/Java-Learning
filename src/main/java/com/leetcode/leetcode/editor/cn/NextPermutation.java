94f25801a846ee937c8a4c1c9c9b229b580d1306
Taoweidong <taowd@outlook.com>
2020-04-23 00:00:38 +0800

    下一个排列

--- src/main/java/com/leetcode/leetcode/editor/cn/NextPermutation.java
+++ src/main/java/com/leetcode/leetcode/editor/cn/NextPermutation.java
@@ -14,22 +14,37 @@ package com.leetcode.leetcode.editor.cn;
 
 /**
  * 下一个排列
+ *
  * @author Taoweidong
  */
 public class NextPermutation {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Solution solution = new NextPermutation().new Solution();
-	}
+        Solution solution = new NextPermutation().new Solution();
+    }
 
-	// leetcode submit region begin(Prohibit modification and deletion)
-	class Solution {
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
 
-		public void nextPermutation(int[] nums) {
+        /**
+         * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+         * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+         * 必须原地修改，只允许使用额外常数空间。
+         * <p>
+         * 题干的意思是：找出这个数组排序出的所有数中，刚好比当前数大的那个数。
+         * <p>
+         * 比如当前 nums = [1,2,3]。这个数是123，找出1，2，3这3个数字排序可能的所有数，排序后，比123大的那个数 也就是132
+         * <p>
+         * 如果当前 nums = [3,2,1]。这就是1，2，3所有排序中最大的那个数，那么就返回1，2，3排序后所有数中最小的那个，也就是1，2，3 -> [1,2,3]
+         * <p>
+         *
+         * @param nums
+         */
+        public void nextPermutation(int[] nums) {
 
-		}
-	}
-	// leetcode submit region end(Prohibit modification and deletion)
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
