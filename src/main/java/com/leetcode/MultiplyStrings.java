53f8a89ca27c6a9317d0e3e33d25c00705226a62
taoweidong <taowd@outlook.com>
2020-05-12 23:33:22 +0800

    字符串相乘

--- src/main/java/com/leetcode/MultiplyStrings.java
+++ src/main/java/com/leetcode/MultiplyStrings.java
@@ -24,55 +24,56 @@ package com.leetcode;
 
 /**
  * 字符串相乘
+ *
  * @author Taoweidong
  */
 public class MultiplyStrings {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Solution solution = new MultiplyStrings().new Solution();
-		System.out.println(solution.multiply("123", "234"));
-	}
+        Solution solution = new MultiplyStrings().new Solution();
+        System.out.println(solution.multiply("123", "234"));
+    }
 
-	// leetcode submit region begin(Prohibit modification and deletion)
-	class Solution {
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
 
-		public String multiply(String num1, String num2) {
+        public String multiply(String num1, String num2) {
 
-			// 处理特殊情况的计算结果
-			if ("0".equals(num1) || "0".equals(num2)) {
-				return "0";
-			}
+            // 处理特殊情况的计算结果
+            if ("0".equals(num1) || "0".equals(num2)) {
+                return "0";
+            }
 
-			int[] res = new int[num1.length() + num2.length()];
+            int[] res = new int[num1.length() + num2.length()];
 
-			for (int i = num1.length() - 1; i >= 0; i--) {
-				// 计算出第一个乘子最右边的数字
-				int n1 = num1.charAt(i) - '0';
-				for (int j = num2.length() - 1; j >= 0; j--) {
-					// 计算第二个乘子最右边的数字
-					int n2 = num2.charAt(j) - '0';
-					int sum = (res[i + j + 1] + n1 * n2);
+            for (int i = num1.length() - 1; i >= 0; i--) {
+                // 计算出第一个乘子最右边的数字
+                int n1 = num1.charAt(i) - '0';
+                for (int j = num2.length() - 1; j >= 0; j--) {
+                    // 计算第二个乘子最右边的数字
+                    int n2 = num2.charAt(j) - '0';
+                    int sum = res[i + j + 1] + n1 * n2;
 
-					// 取余
-					res[i + j + 1] = sum % 10;
-					// 取整:进位运算
-					res[i + j] += sum / 10;
-				}
-			}
+                    // 取余
+                    res[i + j + 1] = sum % 10;
+                    // 取整:进位运算
+                    res[i + j] += sum / 10;
+                }
+            }
 
-			// 处理结果，输出结果
-			StringBuilder result = new StringBuilder();
-			for (int i = 0; i < res.length; i++) {
-				if (i == 0 && res[i] == 0) {
-					continue;
-				} else {
-					result.append(res[i]);
-				}
-			}
-			return result.toString();
-		}
-	}
-	// leetcode submit region end(Prohibit modification and deletion)
+            // 处理结果，输出结果
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < res.length; i++) {
+                if (i == 0 && res[i] == 0) {
+                    continue;
+                } else {
+                    result.append(res[i]);
+                }
+            }
+            return result.toString();
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
