07782b0b5131fa70c8e4357243d6a3bb8ef250fb
Taoweidong <taowd@outlook.com>
2020-06-07 11:46:33 +0800

    完成计算堆栈中的剩余数字

--- src/main/java/com/huawei/oo/Huawei_20200606_02.java
+++ src/main/java/com/huawei/oo/Huawei_20200606_02.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * # 【计算堆栈中的剩余数字】--未完成
+ * # 【计算堆栈中的剩余数字】--完成
  * # 计算堆栈中的剩余数字
  * > 向一个空栈中一次输入正整数，假设入栈元素N（1<=N<=2^31-1）,按顺序以此为Nx...N4、N3、N2、N1，每当元素入栈时，如果N1=N2+，，，+Ny（y的范围[2,x],1<=x<=1000）,则N1~Ny全部元素出栈，重新入栈新元素M（M=2*N1）。
  * 如以此向栈中存入6，1，2，3，当存入6，1，2时，栈底至栈顶依次为[6,1,2],当存入3时，3=2+1，3，2，1全部出栈，重新入栈元素6（6=2*3），此时栈中有元素6，因为6=6，所以两个6全部出栈，存入12，最终栈中只剩下一个元素12。
@@ -23,158 +23,77 @@ import java.util.Stack;
  * 输出：
  * 1 170
  * <p>
- * 6 2 3 5 8
- * 5 2 3 5
- * 5 10 20 50 85 1
+ * 输入：6 2 3 5 8  输出：8 10 6
+ * 输入：5 2 3 5  输出：5 10
+ * 输入：5 10 20 50 85 1 输出：1 170
+ * 输入：10 20 30 60 输出：1 170
  */
 public class Huawei_20200606_02 {
     public static void main(String[] args) {
-
-//        Stack<Integer> tempS = new Stack<>();
-//        tempS.add(5);
-//        tempS.add(2);
-//        tempS.add(3);
-//        tempS.add(5);
-//
-//
-//        getNewStack2(tempS);
-
-//        for (int i = 0; i < tempS.size(); i++) {
-//            System.out.println(tempS.get(i));
-//        }
-
-
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String[] input = sc.nextLine().trim().split(" ");
 
             Stack<Integer> stack = new Stack<>();
             for (int i = 0; i < input.length; i++) {
-
                 //  检查栈中的元素是否等于当前入栈的数据
-                stack.add(Integer.valueOf(input[i]));
-                getNewStack2(stack);
-
+                stack = getNewStack(stack, Integer.valueOf(input[i]));
             }
 
-
+            //处理输出结果
             List<String> output = new ArrayList<>(stack.size());
             while (!stack.isEmpty()) {
                 output.add(String.valueOf(stack.pop()));
             }
-
             System.out.println(String.join(" ", output));
         }
     }
 
-    private static Stack<Integer> getNewStack2(Stack<Integer> stack) {
 
-        //  6 2 3 5 8
-        //  5 2 3 5
-        //  5 10 20 50 85 1
-        //开始逻辑处理，不断出栈相加判断是否满足条件，如果满足则更新结果值
-//        TODO 待完善
-        int start = stack.peek();
-        int index = stack.pop();
-
-        while (!stack.isEmpty()) {
-
-            int count = 0;
-            int indexCount = 0;
-            for (int i = stack.size() - 1; i >= 0; i--) {
-                if (count == index) {
-                    break;
+    private static Stack<Integer> getNewStack(Stack<Integer> stack, Integer s) {
+        //输入：6 2 3 5 8 输出：8 10 6
+        //输入：5 2 3 5 输出：5 10
+        //输入：5 10 20 50 85 1 输出：1 170
+        //输入：10 20 30 60 输出：1 170
+        int count = 0;
+        //对栈中的每一个元素进行处理
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            count += stack.get(i);
+            if (count == s) {
+                //倒数删除元素到索引为i的地方，返回新栈
+                stack = getDeleteStack(stack, i);
+                s = count * 2;
+                if (!stack.isEmpty()) {
+                    //递归进行一次处理
+                    getNewStack(stack, s);
                 } else {
-                    count += stack.get(i);
-                    indexCount++;
+                    stack.add(s);
                 }
-            }
-
-
-            //删除栈顶元素的个数
-            if (count == index) {
-                while (!stack.isEmpty()) {
-                    for (int i = 0; i < indexCount; i++) {
-                        stack.pop();
-                    }
-                    break;
-                }
-                //把新元素添加进去
-                stack.add(index * 2);
-
-            }
-
-
-            if (stack.size() == 1) {
-                if (stack.peek() == index) {
-                    stack.pop();
-                    stack.add(index * 2);
-                } else {
-                    stack.add(index);
-                }
-                break;
-            }
-
-            if (indexCount != stack.size()) {
-                index = stack.pop();
-            } else {
-                break;
+                return stack;
             }
 
         }
-
-        if (stack.isEmpty()) {
-            stack.add(index);
-        }
-
+        //遍历结束没有一个符合条件的，就把s加入到栈中
+        stack.add(s);
         return stack;
 
 
     }
 
-    private static Stack<Integer> getNewStack(Stack<Integer> stack, Integer s) {
-        //记录原始数据，当已经入栈的所有元素均不满足条件时进行处理
-        Stack<Integer> originStack = new Stack<>();
-        originStack.addAll(stack);
-        //处理结果，当满足条件时，会当这个结果集进行更新，最终以此结果答案返回
-        Stack<Integer> result = new Stack<>();
-        if (stack.isEmpty()) {
-            result.add(s);
-            return result;
+    /**
+     * 从栈的后面开始删除，删除到索引位置为deleteIndex位置
+     *
+     * @param stack       待删除栈
+     * @param deleteIndex 删除的索引
+     * @return 新的栈
+     */
+    private static Stack<Integer> getDeleteStack(Stack<Integer> stack, int deleteIndex) {
+
+        for (int j = stack.size() - 1; j >= deleteIndex; j--) {
+            stack.remove(j);
         }
-        result.addAll(stack);
 
-        //  6 2 3 5 8
-        //  5 2 3 5
-        //  5 10 20 50 85 1
-        //开始逻辑处理，不断出栈相加判断是否满足条件，如果满足则更新结果值
-//        TODO 待完善
-        Stack<Integer> tempS = new Stack<>();
-        int count = 0;
-        while (!result.isEmpty()) {
-            if (count == s) {
-//                result.add(count * 2);
-                s = count * 2;
-                tempS.clear();
-                tempS.add(s);
-                count = 0;
-                continue;
-            }
-            int a = result.pop();
-            count += a;
-            tempS.add(a);
-        }
-        tempS.add(s);
-        return tempS;
-
-//        if (count == s) {
-//            result.add(count * 2);
-//            return result;
-//        } else {
-//            originStack.add(s);
-//            return originStack;
-//        }
-
+        return stack;
     }
 
 
