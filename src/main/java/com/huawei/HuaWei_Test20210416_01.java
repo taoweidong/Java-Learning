0224f6163d6f2d2132de7a346094a691d2fb0ba4
Taoweidong <taowd@outlook.com>
2021-04-19 00:00:09 +0800

    二叉搜索树问题-删除节点问题

--- src/main/java/com/huawei/HuaWei_Test20210416_01.java
+++ src/main/java/com/huawei/HuaWei_Test20210416_01.java
@@ -105,6 +105,40 @@ public class HuaWei_Test20210416_01 {
       return root;
     }
   }
+
+  public TreeNode deleteNode(TreeNode root, int key) {
+    if (root == null) {
+      return null;
+    }
+
+    if (root.val == key) {
+      //  找到了待删除的节点信息，删除节点
+      if (root.left == null) {
+        return root.right;
+      }
+      if (root.right == null) {
+        return root.left;
+      }
+
+      TreeNode minNode = getMin(root.right);
+      root.val = minNode.val;
+      root.right = deleteNode(root.right, minNode.val);
+
+    } else if (root.val > key) {
+      root.left = deleteNode(root.left, key);
+    } else if (root.val < key) {
+      root.right = deleteNode(root.right, key);
+    }
+
+    return root;
+  }
+
+  private TreeNode getMin(TreeNode node) {
+    while (node.left != null) {
+      node = node.left;
+    }
+    return node;
+  }
 }
 
 class TreeNode {
