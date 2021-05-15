c691d44df8134b546601d97cc754e335835f2a4e
Taoweidong <taowd@outlook.com>
2021-05-15 13:29:30 +0800

    增加Hutool库的学习依赖

---
+++ src/main/java/com/HuToolSimple.java
@@ -0,0 +1,12 @@
+package com;
+
+import cn.hutool.core.util.RuntimeUtil;
+
+public class HuToolSimple {
+  public static void main(String[] args) {
+    String str = RuntimeUtil.execForStr("java -version");
+    System.out.println(str);
+
+    System.out.println(RuntimeUtil.execForLines("java -version"));
+  }
+}
