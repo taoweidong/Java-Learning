4f4435a44ac4986bfaf604fc52932c9ef90c9188
Taoweidong <taowd@outlook.com>
2021-07-09 00:19:31 +0800

    广度优先遍历

--- src/main/java/com/leetcode/LeetCode_993_IsCousins.java
+++ src/main/java/com/leetcode/LeetCode_993_IsCousins.java
@@ -17,34 +17,56 @@ public class LeetCode_993_IsCousins {
     deque.add(root);
 
     int x_index = 0;
+    TreeNode x_parent = null;
     int y_index = 0;
+    TreeNode y_parent = null;
+
     int index = 1;
     while (!deque.isEmpty()) {
       int count = deque.size();
 
       for (int i = 0; i < count; i++) {
         TreeNode current = deque.poll();
-        if (current.val == x) {
-          x_index = index;
-        }
-        if (current.val == y) {
-          y_index = index;
-        }
 
         if (current.left != null) {
+
+          if (current.left.val == x) {
+            x_index = index;
+            x_parent = current;
+            continue;
+          }
+          if (current.left.val == y) {
+            y_index = index;
+            y_parent = current;
+            continue;
+          }
+
           deque.add(current.left);
         }
         if (current.right != null) {
+
+          if (current.right.val == x) {
+
+            x_index = index;
+            x_parent = current;
+            continue;
+          }
+          if (current.right.val == y) {
+            y_index = index;
+            y_parent = current;
+            continue;
+          }
           deque.add(current.right);
         }
       }
       index++;
     }
 
-    if (x_index == y_index && x_index > 2) {
-      return true;
+    if (y_parent != x_parent) {
+      if (x_index == y_index) {
+        return true;
+      }
     }
-
     return false;
   }
 }
