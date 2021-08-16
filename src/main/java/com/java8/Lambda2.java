ab11aac01a481ffd63f9059b9cd66842d8fc4903
Taoweidong <taowd@outlook.com>
2021-08-16 23:09:29 +0800

    lambda表达式学习

--- src/main/java/com/java8/Lambda2.java
+++ src/main/java/com/java8/Lambda2.java
@@ -1,9 +1,13 @@
 package com.java8;
 
 import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 /**
+ * Lambda 是一个 匿名函数，我们可以把 Lambda 表达式理解为是 一段可以传递的代码（将代码 像数据一样进行传递）。<br/>
+ * 可以写出更简洁、更 灵活的代码。作为一种更紧凑的代码风格，使 Java的语言表达能力得到了提升。
+ * 
  * @author Taowd
  * @date 2018年12月19日 <br/>
  *       左侧：参数列表<br/>
@@ -12,46 +16,38 @@ import java.util.TreeSet;
 @SuppressWarnings("unused")
 public class Lambda2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// 匿名内部类
+        // 多线程实现方式
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello");
+            }
+        };
 
-		Runnable r1 = new Runnable() {
+        Thread thread = new Thread(r1);
+        thread.start();
+        System.out.println("************************");
 
-			@Override
-			public void run() {
+        Runnable r2 = () -> System.out.println("World!!!");
+        Thread thread1 = new Thread(r2);
+        thread1.start();
+        System.out.println("************************");
 
-				// System.out.println("Test");
-			}
-		};
+        PriorityQueue<String> queue = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return 0;
+            }
+        });
 
-		// Lambda表达式
-		Runnable r2 = () -> System.out.println("Test");
+        PriorityQueue<String> queue2 = new PriorityQueue<>((o1, o2) -> {
+            return -1;
+        });
 
-		// 原来的使用匿名内部类作为参数传递
-		TreeSet<String> ts = new TreeSet<>(new Comparator<String>() {
+        new LinkedList<String>();
 
-			@Override
-			public int compare(String o1, String o2) {
-
-				// TODO Auto-generated method stub
-				return Integer.compare(o1.length(), o2.length());
-			}
-		});
-
-		// Lambda表达式作为参数传递
-		TreeSet<String> ts2 = new TreeSet<>((o1, o2) -> Integer.compare(o1.length(), o2.length()));
-
-		String result = new Lambda2().toUpper(() -> {
-			return 12;
-		}, "Hello World!");
-		System.out.println(result);
-
-	}
-
-	public String toUpper(MyNumber my, String str) {
-
-		return my.getValue() + str;
-	}
+    }
 
 }
