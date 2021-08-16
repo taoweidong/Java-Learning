a66b3d73e72905eeb0398f68c488c62950ecf7f1
Taoweidong <taowd@outlook.com>
2021-08-17 06:40:05 +0800

    代码格式优化

--- src/main/java/com/leetcode/AddTwoNumbers.java
+++ src/main/java/com/leetcode/AddTwoNumbers.java
@@ -6,8 +6,8 @@ import java.util.List;
 import com.alibaba.fastjson.JSON;
 
 /**
- * 题目描述：给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
- * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。<br/>
+ * 题目描述：给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 您可以假设除了数字
+ * 0 之外，这两个数都不会以 0 开头。<br/>
  * 时间复杂度：O(max⁡(m,n))，假设 mmm 和 nnn 分别表示 l1l1l1 和 l2l2l2 的长度，上面的算法最多重复 max⁡(m,n)次。<br/>
  * 空间复杂度：O(max⁡(m,n))， 新列表的长度最多为 max⁡(m,n)+1。<br/>
  *
@@ -45,12 +45,14 @@ public class AddTwoNumbers {
     /**
      * 计算链表之和
      *
-     * @param l1 链表1
-     * @param l2 链表2
+     * @param l1
+     *            链表1
+     * @param l2
+     *            链表2
      * @return 相加结果
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //定义一个头结点，用来存放计算的结果
+        // 定义一个头结点，用来存放计算的结果
         ListNode dumHead = new ListNode(0);
         ListNode p = l1, q = l2, curr = dumHead;
         // 存放进位的值
@@ -60,8 +62,8 @@ public class AddTwoNumbers {
             int y = (q != null) ? q.val : 0;
             int sum = x + y + carry;
             // 取整
-            //carry = sum / 10;
-            //curr.next = new ListNode(sum % 10);
+            // carry = sum / 10;
+            // curr.next = new ListNode(sum % 10);
             carry = sum >= 10 ? 1 : 0;
             curr.next = new ListNode(sum >= 10 ? sum - 10 : sum);
             // 最终结果的一位的值
@@ -98,6 +100,5 @@ class ListNode {
     ListNode(int x) {
         val = x;
     }
-
 
 }
